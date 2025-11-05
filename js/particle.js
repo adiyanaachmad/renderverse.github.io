1266d30b8a298d3323fd917d568542da16166267
adiyanaachmad <acreative3179@gmail.com>
2025-11-05 23:35:42 +0700

    Update 0.1.6 (1)

---
+++ js/particle.js
@@ -0,0 +1,37 @@
+particlesJS("particles-js", {
+    "particles": {
+        "number": {
+            "value": 20, 
+            "density": {
+                "enable": true,
+                "value_area": 800
+            }
+        },
+        "color": {
+            "value": "#FEFEFE"
+        },
+        "line_linked": {
+            "enable": true, 
+            "distance": 100, 
+            "color": "#ffffff",
+            "opacity": 0.5,
+            "width": 2
+        },
+        "shape": {
+            "type": "circle"
+        },
+        "opacity": {
+            "value": 1.0
+        },
+        "size": {
+            "value": 5, // Gunakan nilai default 5
+            "random": false
+        },
+        "move": {
+            "enable": true,
+            "speed": 5,
+            "direction": "none"
+        }
+    },
+    "retina_detect": false
+});
