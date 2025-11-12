92f76336bd0be5a677832dca98f5881e3f75511a
adiyanaachmad <acreative3179@gmail.com>
2025-11-12 23:53:32 +0700

    Update 0.1.8 (2)

--- js/particle.js
+++ js/particle.js
@@ -13,16 +13,16 @@ const particlePresets = {
     'random': {
         "number": { "value": 20, "density": { "enable": true, "value_area": 800 } },
         "color": { "value": "#FEFEFE" },
-        "line_linked": { "enable": true, "distance": 100, "color": "#ffffff", "opacity": 0.5, "width": 2 },
+        "line_linked": { "enable": false},
         "shape": { "type": "circle" },
-        "opacity": { "value": 1.0 },
-        "size": { "value": 5, "random": false },
-        "move": { "enable": true, "speed": 5, "direction": "none", "random": false, "straight": false, "out_mode": "out", "bounce": false, "attract": { "enable": false, "rotateX": 600, "rotateY": 1200 } }
+        "opacity": { "value": 0.8, "random": true },
+        "size": { "value": 3, "random": true },
+        "move": { "enable": true, "speed": 5, "direction": "none", "random": true, "straight": false, "out_mode": "out", "bounce": false, "attract": { "enable": false, "rotateX": 600, "rotateY": 1200 } }
     },
     // Mode Snow (Contoh konfigurasi salju/turun ke bawah)
     'snow': {
-        "number": { "value": 50, "density": { "enable": true, "value_area": 800 } },
-        "color": { "value": "#FFFFFF" },
+        "number": { "value": 20, "density": { "enable": true, "value_area": 800 } },
+        "color": { "value": "#FEFEFE" },
         "line_linked": { "enable": false }, // Salju biasanya tidak terhubung
         "shape": { "type": "circle" },
         "opacity": { "value": 0.8, "random": true },
