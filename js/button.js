101243a76596dce7caa25d8d0d744aecd10bab6f
adiyanaachmad <acreative3179@gmail.com>
2025-12-17 13:00:52 +0700

    Update 0.2.0 (10)

--- js/button.js
+++ js/button.js
@@ -18,13 +18,13 @@ document.querySelectorAll('.set-jut').forEach(btn => {
 
 const audioContext = new (window.AudioContext || window.webkitAudioContext)();
 const gainNode = audioContext.createGain();
-gainNode.gain.value = 5.0; 
+gainNode.gain.value = 5.0;
 
 gainNode.connect(audioContext.destination);
 
 // 4. Memuat file suara
 const clickSound = new Audio('sounds/684505__saha213131__click.mp3');
-clickSound.crossOrigin = "anonymous"; 
+clickSound.crossOrigin = "anonymous";
 
 let source;
 clickSound.addEventListener('canplaythrough', () => {
@@ -34,7 +34,7 @@ clickSound.addEventListener('canplaythrough', () => {
 
 function playSoundEffect() {
   if (audioContext.state === 'suspended') {
-      audioContext.resume();
+    audioContext.resume();
   }
   clickSound.currentTime = 0;
   clickSound.play().catch(error => {
@@ -92,3 +92,47 @@ function toggleFullscreen() {
 
 // Menambahkan event listener untuk tombol maximize
 maximizeBtn.addEventListener("click", toggleFullscreen);
+
+
+const setgToggle = document.getElementById('setg-toggle');
+const setgContainer = document.getElementById('setpi');
+const cardBottomContainer = document.querySelector('.card-bottom-container');
+const toggleIcon = document.getElementById('toggle-icon');
+
+setgToggle.addEventListener('click', function () {
+  // Toggle visibility of setg-container
+  setgContainer.classList.toggle('active');
+
+  // Toggle visibility of card-bottom-container
+  if (setgContainer.classList.contains('active')) {
+    cardBottomContainer.style.display = 'none';  // Hide the card-bottom-container
+    toggleIcon.classList.remove('fa-gear');      // Remove the gear icon
+    toggleIcon.classList.add('fa-times');        // Add the close icon
+  } else {
+    cardBottomContainer.style.display = 'flex'; // Show the card-bottom-container
+    toggleIcon.classList.remove('fa-times');     // Remove the close icon
+    toggleIcon.classList.add('fa-gear');          // Add the gear icon
+  }
+});
+
+const btn = document.getElementById('set-des');
+const bottomSelect = document.querySelector('.bottom-select');
+const btnIcon = document.getElementById('btn-icon');
+
+let isOpen = false;  // Menandakan apakah bottom-select sudah terbuka atau belum
+
+btn.addEventListener('click', function() {
+    if (isOpen) {
+        // Kembalikan posisi bottom ke 90px dan sembunyikan bottom-select
+        bottomSelect.style.bottom = '90px';
+        btnIcon.classList.remove('rotated');
+    } else {
+        // Ubah posisi bottom menjadi 20px dan tampilkan bottom-select
+        bottomSelect.style.bottom = '10px';
+        bottomSelect.style.borderRadius = '10px';
+        btnIcon.classList.add('rotated');
+    }
+
+    // Toggle status
+    isOpen = !isOpen;
+});
